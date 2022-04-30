cd014c134082ff81cab42e17efa3da513590ba19
zzhhdaaa <zzhhdaaa@g.ucla.edu>
2022-04-30 02:05:31 -0700

    element placement

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -6,6 +6,7 @@ public class CursorMovement : MonoBehaviour
 {
     RaycastHit hit;
     Ray ray;
+    GridElement lastHit;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,71 @@ public class CursorMovement : MonoBehaviour
         if (Physics.Raycast(ray, out hit) && hit.collider.tag == "GridElement")
         {
             transform.position = hit.collider.transform.position;
+            lastHit = hit.collider.GetComponent<GridElement>();
+            if (Input.GetMouseButtonDown(1))
+            {
+                SetCursorButton(6);
+            }
+        }
+    }
+
+    public void SetCursorButton(int input)
+    {
+        Coord coord = lastHit.GetCoord();
+        int gridX = LevelGenerator.instance.gridX;
+        int gridY = LevelGenerator.instance.gridY;
+        int gridZ = LevelGenerator.instance.gridZ;
+
+        switch (input)
+        {
+            case 0:
+                //Z+
+                if (coord.z < gridZ - 1)
+                {
+                    LevelGenerator.instance.gridElements[coord.x * gridY * gridZ + coord.y * gridZ + (coord.z + 1)].SetEnable();
+                }
+                break;
+            case 1:
+                //Z-
+                if (coord.z > 0)
+                {
+                    LevelGenerator.instance.gridElements[coord.x * gridY * gridZ + coord.y * gridZ + (coord.z - 1)].SetEnable();
+                }
+                break;
+            case 2:
+                //Y+
+                if (coord.y < gridY - 1)
+                {
+                    LevelGenerator.instance.gridElements[coord.x * gridY * gridZ + (coord.y + 1) * gridZ + coord.z].SetEnable();
+                }
+                break;
+            case 3:
+                //Y-
+                if (coord.y > 0)
+                {
+                    LevelGenerator.instance.gridElements[coord.x * gridY * gridZ + (coord.y - 1) * gridZ + coord.z].SetEnable();
+                }
+                break;
+            case 4:
+                //X+
+                if (coord.x < gridX - 1)
+                {
+                    LevelGenerator.instance.gridElements[(coord.x + 1) * gridY * gridZ + coord.y * gridZ + coord.z].SetEnable();
+                }
+                break;
+            case 5:
+                //X-
+                if (coord.x > 0)
+                {
+                    LevelGenerator.instance.gridElements[(coord.x - 1) * gridY * gridZ + coord.y * gridZ + coord.z].SetEnable();
+                }
+                break;
+            case 6:
+                if (coord.y > 0)
+                {
+                    lastHit.SetDisable();
+                }
+                break;
         }
     }
 }
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    int gridX = 5;
-    int gridY = 10;
-    int gridZ = 5;
+    public static LevelGenerator instance;
+
+    public int gridX = 5;
+    public int gridY = 10;
+    public int gridZ = 5;
     public GridElement gridElement;
     public GridElement[] gridElements;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
         gridElements = new GridElement[gridX * gridY * gridZ];
 
         for (int x = 0; x < gridX; x++)
@@ -22,7 +33,7 @@ public class LevelGenerator : MonoBehaviour
                 for (int z = 0; z < gridZ; z++)
                 {
                     GridElement gridElementInstance = Instantiate(gridElement, new Vector3(x, y, z), Quaternion.identity, this.transform);
-                    gridElementInstance.name = x + "_" + y + "_" + z;
+                    gridElementInstance.Initialize(x, y, z);
                     gridElements[x * gridY *gridZ + y * gridZ + z] = gridElementInstance;
                 }
             }
